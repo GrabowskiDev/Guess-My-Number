d6f64a79f6c0cf82e72ea07298f18439a5edb7cd
Łukasz Grabowski <lukaszgra2004@gmail.com>
2021-10-15 22:26:17 +0200

    add basic logic

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 'use strict';
+
+//Set random number in range
+function randomNumber(range) {
+	return Math.trunc(Math.random() * range) + 1;
+}
+
+function start() {}
+
+//Increase and decrease that value on btns press
+
+console.log(range);
+//Score and highscore system
+//Some animations
+const guess = document.querySelector('#guess');
+const rangeInput = document.querySelector('#range');
+const mark = document.querySelector('#mark');
+
+//On pageload:
+let number = randomNumber(20);
+
+//Event listeners:
+//Check if greater or worse than (and display >, <)
+document.querySelector('#check').addEventListener('click', () => {
+	if (!guess.value) {
+		alert('Insert a number first');
+	} else if (guess.value > number) {
+		mark.textContent = '>';
+	} else if (guess.value < number) {
+		mark.textContent = '<';
+	} else if (guess.value == number) {
+		mark.textContent = '=';
+		document.querySelector('#number').textContent = number;
+		document.querySelector('#check').disabled = true;
+	}
+});
